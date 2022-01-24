e4d77de79f5f92aff044fa6d61fc3965dfeb4a0e
Yıldıray TAŞBAŞ <yildirayt@idealdata.com.tr>
2022-01-24 12:20:11 +0300

    services

--- src/app/component/product/product.component.ts
+++ src/app/component/product/product.component.ts
@@ -1,5 +1,8 @@
+import { ProductService } from './../../services/product.service';
+import { ProductResponseModel } from './../../models/productResponseModel';
 import { Product } from './../../models/product';
 import { Component, OnInit } from '@angular/core';
+
 
 @Component({
   selector: 'app-product',
@@ -7,12 +10,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product.component.css']
 })
 export class ProductComponent implements OnInit {
+  products:Product[]=[];
+  dataLoaded=false;
 
-  products:Product[] = []
+  // productResponseModel:ProductResponseModel={
+  //   data:this.products,
+  //   message:"",
+  //   success:true
+  // };
 
-  constructor() { }
+  constructor(private productService:ProductService) { }
 
   ngOnInit(): void {
+      this.getProducts()
+  }
+
+  getProducts(){
+    this.productService.getProducts().subscribe(response =>
+      {
+        this.products=response.data;
+        this.dataLoaded=true;
+      })
+
   }
 
 }
